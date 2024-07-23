577f07849c22aacf0c883d2af4d3c432bdd37924
Timofey <tim.af01@yandex.ru>
2024-07-23 14:08:11 +0300

    added db creator for fist launch n updated requirements

---
+++ app/db_creator.py
@@ -0,0 +1,10 @@
+from app.db_requests import create_tables, create_test
+from app.database import sync_engine
+
+from sqlalchemy_utils import database_exists, create_database
+
+if __name__ == '__main__':
+    if not database_exists(sync_engine.url):
+        create_database(sync_engine.url)
+    create_tables()
+    create_test()
